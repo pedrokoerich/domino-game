21ffbe3758cd5dee9d1ffd4b117755b8af9e42b4
Pedro Koerich <pedro.koerich@totvs.com.br>
2024-04-03 22:14:00 -0300

    desenvolvido arrasta e solta das peças

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDragStart, CdkDrag, CdkDropList, DragRef, Point, copyArrayItem } from '@angular/cdk/drag-drop';
+
+interface PiecesInPlayArea {
+  [key: string]: [number, number][];
+}
 
 @Component({
   selector: 'app-root',
@@ -8,7 +13,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'domino-game';
   public player1: Array<[number, number]> = []; 
-  public player2: Array<[number, number]> = []; 
+  public player2: Array<[number, number]> = [];
+  public playAreaData: Array<[number, number]> = []; 
+  piecesInPlayArea: PiecesInPlayArea = {};
+  isPieceInPlayArea = false;
 
   // Função para gerar todas as combinações possíveis das peças de dominó
   generateDominoPieces(): Array<[number, number]> {
@@ -23,13 +31,37 @@ export class AppComponent {
 
   // Função para distribuir as peças entre os jogadores e as restantes para compra
   distributePieces(allPieces: Array<[number, number]>, numPieces: number): { player1: Array<[number, number]>, player2: Array<[number, number]>, buy: Array<[number, number]> } {
-    const shuffledPieces = allPieces.sort(() => Math.random() - 0.5); // Embaralha as peças
-
+    const shuffledPieces = allPieces.sort(() => Math.random() - 0.5);
+  
     const player1 = shuffledPieces.slice(0, numPieces);
     const player2 = shuffledPieces.slice(numPieces, numPieces * 2);
     const buy = shuffledPieces.slice(numPieces * 2);
-
+  
     return { player1, player2, buy };
+  }
+  
+
+  onPieceDropped(event: CdkDragDrop<[number, number][]>): void {
+    const droppedElement = event.event.target as HTMLElement;
+    const droppedElementId = droppedElement.id;
+    
+    if (droppedElementId && droppedElementId == 'play-area') {
+      this.isPieceInPlayArea = true;
+      const pieceIndex = this.player1.indexOf(event.item.data);
+      if (pieceIndex !== -1) {
+        this.player1.splice(pieceIndex, 1); // Remover a peça do jogador 1
+      } else {
+        const pieceIndex = this.player2.indexOf(event.item.data);
+        if (pieceIndex !== -1) {
+          this.player2.splice(pieceIndex, 1); // Remover a peça do jogador 2
+        }
+      }
+      this.piecesInPlayArea['position_' + Object.keys(this.piecesInPlayArea).length] = event.item.data;
+    } else {
+      this.isPieceInPlayArea = false;
+      // Lógica para lidar com a solta em outras áreas, se necessário
+    }
+    console.log(this.piecesInPlayArea)
   }
 
   ngOnInit() {
@@ -39,6 +71,7 @@ export class AppComponent {
 
     this.player1 = player1; // Atribui as peças do jogador 1 à propriedade player1
     this.player2 = player2; // Atribui as peças do jogador 2 à propriedade player2
+    this.playAreaData = buy; // Atribui as peças restantes para a área de jogo
 
     console.log("Peças do jogador 1:", player1);
     console.log("Peças do jogador 2:", player2);
