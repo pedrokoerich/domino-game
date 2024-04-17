12f0656e0d2b637383c903387bf063fe0207c814
Pedro Koerich <pedro.koerich@totvs.com.br>
2024-04-17 20:03:12 -0300

    Ajusatda lógica de rotação das peças e criado html do botão de compra de peças

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, CdkDragStart, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+
+interface DominoPiece {
+  piece: [number, number];
+  horizontal: boolean;
+}
 
 @Component({
   selector: 'app-root',
@@ -8,15 +13,16 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/dr
 })
 export class AppComponent implements OnInit {
   title = 'domino-game';
-  public player1: Array<[number, number]> = [];
-  public player2: Array<[number, number]> = [];
-  public peca: Array<[number, number]> = [];
-  public playAreaData: Array<[number, number]> = [];
-  done: Array<[number, number]> = [];
+  player1: DominoPiece[] = [];
+  player2: DominoPiece[] = [];
+  peca: [number, number][] = [];
+  playAreaData: [number, number][] = [];
+  done: DominoPiece[] = [];
+  horizontalPiece = false;
+  draggedPieceIndex: number | null = null;
 
-  // Função para gerar todas as combinações possíveis das peças de dominó
-  generateDominoPieces(): Array<[number, number]> {
-    let pieces: Array<[number, number]> = [];
+  generateDominoPieces(): [number, number][] {
+    const pieces: [number, number][] = [];
     for (let i = 0; i <= 6; i++) {
       for (let j = i; j <= 6; j++) {
         pieces.push([i, j]);
@@ -27,109 +33,124 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     const allPieces = this.generateDominoPieces();
-    const numPiecesPerPlayer = 7; // Número de peças por jogador
+    const numPiecesPerPlayer = 7;
     const { player1, player2, buy } = this.distributePieces(allPieces, numPiecesPerPlayer);
 
-    this.player1 = player1; // Atribui as peças do jogador 1 à propriedade player1
-    this.player2 = player2; // Atribui as peças do jogador 2 à propriedade player2
-    this.playAreaData = buy; // Atribui as peças restantes para a área de jogo
-
-    console.log("Peças do jogador 1:", player1);
-    console.log("Peças do jogador 2:", player2);
-    console.log("Peças para compra:", buy);
+    this.player1 = player1;
+    this.player2 = player2;
+    this.playAreaData = buy;
   }
 
-  // Função para distribuir as peças entre os jogadores e as restantes para compra
-  distributePieces(allPieces: Array<[number, number]>, numPieces: number): { player1: Array<[number, number]>, player2: Array<[number, number]>, buy: Array<[number, number]> } {
+  distributePieces(allPieces: [number, number][], numPieces: number): { player1: DominoPiece[], player2: DominoPiece[], buy: [number, number][] } {
     const shuffledPieces = allPieces.sort(() => Math.random() - 0.5);
-
-    const player1 = shuffledPieces.slice(0, numPieces);
-    const player2 = shuffledPieces.slice(numPieces, numPieces * 2);
+    const player1 = shuffledPieces.slice(0, numPieces).map(piece => ({ piece, horizontal: false }));
+    const player2 = shuffledPieces.slice(numPieces, numPieces * 2).map(piece => ({ piece, horizontal: false }));
     const buy = shuffledPieces.slice(numPieces * 2);
 
     return { player1, player2, buy };
   }
-  drop(event: CdkDragDrop<Array<[number, number]>>) {
+
+  drop(event: CdkDragDrop<DominoPiece[]>) {
+    const droppedItem = event.previousContainer.data[event.previousIndex];
+    const targetPiece = event.container.data[event.currentIndex]?.piece;
+    const droppedLeftSide = droppedItem.piece[0];
+    const droppedRightSide = droppedItem.piece[1];
+
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      const droppedPiece = event.previousContainer.data[event.previousIndex];
-      const targetPiece = event.container.data[event.currentIndex];
-  
-      const droppedLeftSide = droppedPiece[0];
-      const droppedRightSide = droppedPiece[1];
-  
-      // Verifica se a peça de destino não é undefined
-      if (targetPiece) {
-        const targetLeftSide = targetPiece[0];
-        const targetRightSide = targetPiece[1];
-  
-        if (droppedRightSide === targetLeftSide) {
-          // Conectar peças com lados iguais
-          transferArrayItem(
-            event.previousContainer.data,
-            event.container.data,
-            event.previousIndex,
-            event.currentIndex
-          );
-        } else if (droppedLeftSide === targetRightSide) {
-          // Se necessário, gira a peça solta
-          const rotatedPiece: [number, number] = [droppedRightSide, droppedLeftSide];
-          transferArrayItem(
-            event.previousContainer.data,
-            event.container.data,
-            event.previousIndex,
-            event.currentIndex
-          );
-          event.container.data[event.currentIndex] = rotatedPiece;
-        } else {
-          // Se os lados não corresponderem, não permite a conexão
-          console.log("Os lados das peças não correspondem. A conexão não é permitida.");
-        }
+    } else if (targetPiece) {
+      const targetLeftSide = targetPiece[0];
+      const targetRightSide = targetPiece[1];
+
+      if (droppedRightSide === targetLeftSide) {
+        transferArrayItem(
+          event.previousContainer.data,
+          event.container.data,
+          event.previousIndex,
+          event.currentIndex
+        );
+        droppedItem.horizontal = droppedLeftSide !== droppedRightSide;
+      } else if (droppedLeftSide === targetRightSide) {
+        const rotatedPiece: [number, number] = [droppedRightSide, droppedLeftSide];
+        transferArrayItem(
+          event.previousContainer.data,
+          event.container.data,
+          event.previousIndex,
+          event.currentIndex
+        );
+        event.container.data[event.currentIndex].piece = rotatedPiece;
+        droppedItem.horizontal = true;
+      } else if (droppedLeftSide === targetLeftSide) {
+        const attachedPiece: [number, number] = [droppedRightSide, targetRightSide];
+        transferArrayItem(
+          event.previousContainer.data,
+          event.container.data,
+          event.previousIndex,
+          event.currentIndex
+        );
+        event.container.data[event.currentIndex].piece = attachedPiece;
+        droppedItem.horizontal = true;
+      } else if (droppedRightSide === targetRightSide) {
+        const attachedPiece: [number, number] = [targetLeftSide, droppedLeftSide];
+        transferArrayItem(
+          event.previousContainer.data,
+          event.container.data,
+          event.previousIndex,
+          event.currentIndex
+        );
+        event.container.data[event.currentIndex].piece = attachedPiece;
+        droppedItem.horizontal = true;
       } else {
-        // Verifica se a mesa está vazia antes de realizar a verificação
-        if (this.done.length === 0) {
-          // Se a mesa estiver vazia, permite colocar a peça na mesa
-          transferArrayItem(
-            event.previousContainer.data,
-            event.container.data,
-            event.previousIndex,
-            event.currentIndex
-          );
-        } else {
-          // Se a mesa não estiver vazia, verifique se a peça pode ser colocada na mesa
-          let canPlacePiece = false;
-          for (const piece of this.done) {
-            const leftSide = piece[0];
-            const rightSide = piece[1];
-            if (droppedLeftSide === leftSide || droppedRightSide === rightSide) {
-              canPlacePiece = true;
-              break;
-            }
-          }
-  
-          if (canPlacePiece) {
-            transferArrayItem(
-              event.previousContainer.data,
-              event.container.data,
-              event.previousIndex,
-              event.currentIndex
-            );
-          } else {
-            console.log("Não é possível colocar essa peça na mesa.");
-          }
+        console.log("Os lados das peças não correspondem. A conexão não é permitida.");
+      }
+    } else if (this.done.length === 0) {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+      droppedItem.horizontal = droppedLeftSide !== droppedRightSide;
+    } else {
+      let canPlacePiece = false;
+      for (const piece of this.done) {
+        const leftSide = piece.piece[0];
+        const rightSide = piece.piece[1];
+        if (droppedLeftSide === leftSide || droppedRightSide === rightSide) {
+          canPlacePiece = true;
+          break;
+        }
+      }
+
+      if (canPlacePiece) {
+        transferArrayItem(
+          event.previousContainer.data,
+          event.container.data,
+          event.previousIndex,
+          event.currentIndex
+        );
+        droppedItem.horizontal = droppedLeftSide !== droppedRightSide;
+      } else {
+        console.log("Não é possível colocar essa peça na mesa.");
+      }
+    }
+
+    if (this.draggedPieceIndex !== null && event.currentIndex === this.draggedPieceIndex) {
+      if (droppedItem) {
+        const droppedPiece = droppedItem.piece;
+        const droppedLeftSide = droppedPiece[0];
+        const droppedRightSide = droppedPiece[1];
+
+        if (droppedLeftSide !== droppedRightSide) {
+          droppedItem.horizontal = true;
         }
       }
     }
-  }
-  
-
-
-  dropPlayer1(event: CdkDragDrop<Array<[number, number]>>) {
-    moveItemInArray(this.player1, event.previousIndex, event.currentIndex);
+    this.draggedPieceIndex = null;
   }
 
-  dropPlayer2(event: CdkDragDrop<Array<[number, number]>>) {
-    moveItemInArray(this.player2, event.previousIndex, event.currentIndex);
+  dragStarted(event: CdkDragStart) {
+    const draggedPieceId = event.source.element.nativeElement.id;
+    this.draggedPieceIndex = parseInt(draggedPieceId.split('_')[1]);
   }
 }
